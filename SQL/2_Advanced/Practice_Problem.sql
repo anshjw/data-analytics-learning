3a926e5b4821afd40f09059e64c59ead341f9621
anshjw <anshjw1926@gmail.com>
2026-02-11 22:42:32 +0530

    Practice problem 7 SQL

--- SQL/2_Advanced/Practice_Problem.sql
+++ SQL/2_Advanced/Practice_Problem.sql
@@ -26,3 +26,35 @@ CREATE TABLE Mar_jobs AS
         job_postings_fact
     WHERE
         EXTRACT(MONTH FROM job_posted_date) IN (3);
+
+/*
+Practice Problem 2:
+    Find the count of the number of remote job postings per skill
+        - Display the top 5 skills by their demand in remote jobs
+        - Include name, skill_id and count of remote posting requiring the skill
+*/
+WITH remote_job_skills AS (
+    SELECT
+        skill_id,
+        COUNT(*) AS skill_count
+    FROM
+        skills_job_dim
+    INNER JOIN job_postings_fact ON skills_job_dim.job_id = job_postings_fact.job_id
+    WHERE
+        job_postings_fact.job_work_from_home = true AND
+        job_postings_fact.job_title_short = 'Data Analyst'
+    GROUP BY
+        skill_id
+    ORDER BY
+        skill_count DESC
+)
+SELECT
+    skills_dim.skill_id,
+    skills AS skill_name,
+    skill_count
+FROM
+    remote_job_skills
+INNER JOIN skills_dim ON remote_job_skills.skill_id = skills_dim.skill_id
+ORDER BY
+    skill_count DESC
+LIMIT 5;
