7e40adaabec8b740dca90cd0d4d4a64a3679bfff
anshjw <anshjw1926@gmail.com>
2026-02-11 22:08:45 +0530

    Practice SQL

--- SQL/2_Advanced/Practice.sql
+++ SQL/2_Advanced/Practice.sql
@@ -37,11 +37,17 @@ WHERE
 WITH company_job_counts AS (
     SELECT
         company_id,
-        COUNT(*) AS job_openings
+        COUNT(*) AS total_jobs
     FROM
         job_postings_fact
     GROUP BY
         company_id
 )
-SELECT *
-FROM company_job_counts;
+SELECT
+    company_dim.name AS company_name,
+    company_job_counts.total_jobs
+FROM
+    company_dim
+LEFT JOIN company_job_counts ON company_dim.company_id = company_job_counts.company_id
+ORDER BY
+    company_job_counts.total_jobs DESC;
