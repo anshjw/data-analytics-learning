cfeb986eeef443d3e909e4c6d09a25bf27b38540
anshjw <anshjw1926@gmail.com>
2026-02-12 23:05:39 +0530

    Practice problem solution in SQL

--- SQL/2_Advanced/Practice_Problem.sql
+++ SQL/2_Advanced/Practice_Problem.sql
@@ -66,3 +66,31 @@ Find job postings from the first quarter that have a salary greater than 70K
     - Combine job postings tables from the first quarter of 2023
     - Get the job postings with an average yearly salary > 70K
 */
+SELECT
+    job_title_short,
+    job_location,
+    job_via,
+    job_posted_date::DATE,
+    salary_year_avg
+FROM (
+    SELECT *
+    FROM
+        jan_jobs
+
+    UNION ALL
+
+    SELECT *
+    FROM
+        feb_jobs
+
+    UNION ALL
+
+    SELECT *
+    FROM
+        mar_jobs
+) AS quarter_one_jobs
+WHERE
+    salary_year_avg > 70000 AND
+    job_title_short = 'Data Analyst'
+ORDER BY
+    salary_year_avg DESC;
