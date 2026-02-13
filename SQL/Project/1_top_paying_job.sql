bf0c166f8c996df6a66240e169e7990d50628a62
anshjw <anshjw1926@gmail.com>
2026-02-13 23:46:39 +0530

    Query one SQL

--- SQL/Project/1_top_paying_job.sql
+++ SQL/Project/1_top_paying_job.sql
@@ -3,3 +3,18 @@
 - Focuses on job postings with specified salaries (remove nulls)
 - Why? Highlights the top-paying opportunities for Data Analytics
 */
+SELECT
+    job_id,
+    job_title,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date
+FROM
+    job_postings_fact jpf
+LEFT JOIN company_dim cd ON jpf.company_id = cd.company_id
+WHERE
+    job_title = 'Data Analystics' AND
+    job_location = 'Anywhere'AND
+    salary_year_avg <> NULL
+ORDER BY
+    salary_year_avg DESC;
