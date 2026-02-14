0c7c71219737d54b08053167ec6747b544d35c9b
anshjw <anshjw1926@gmail.com>
2026-02-14 22:02:32 +0530

    Query one for SQL project

--- SQL/Project/1_top_paying_job.sql
+++ SQL/Project/1_top_paying_job.sql
@@ -8,13 +8,15 @@ SELECT
     job_title,
     job_schedule_type,
     salary_year_avg,
-    job_posted_date
+    job_posted_date,
+    name AS company
 FROM
-    job_postings_fact jpf
-LEFT JOIN company_dim cd ON jpf.company_id = cd.company_id
+    job_postings_fact
+LEFT JOIN company_dim ON company_dim.company_id = job_postings_fact.company_id
 WHERE
-    job_title = 'Data Analystics' AND
-    job_location = 'Anywhere'AND
-    salary_year_avg <> NULL
+    job_title_short = 'Data Analyst' AND
+    job_location = 'Anywhere' AND
+    salary_year_avg IS NOT NULL
 ORDER BY
-    salary_year_avg DESC;
+    salary_year_avg DESC
+LIMIT 10;
