5b0cdd83d102b04ce058087f83addcaa9568f6fd
anshjw <anshjw1926@gmail.com>
2026-02-14 22:53:03 +0530

    Analysis on the queries of SQL project

--- SQL/Project/2_top_paying_job_skills.sql
+++ SQL/Project/2_top_paying_job_skills.sql
@@ -1,7 +1,7 @@
 /*
 Question: What skills are required for the top-paying data analyst jobs?
-- Use the top 10 highest paying Data analyst job from first query
-- Add the specific skills required for this role
+- Use the top 10 highest paying Data analyst job from first query.
+- Add the specific skills required for this role.
 - Why? It provides a detailed look at which high-paying jobs demand certain skills,
     help helping job seekers understand which skills to develop that align with top salaries
 */
@@ -24,9 +24,42 @@ WITH top_paying_jobs AS (
 )
 
 SELECT
-    top_paying_jobs.*
+    top_paying_jobs.*,
+    skills
 FROM
     top_paying_jobs
 INNER JOIN skills_job_dim ON skills_job_dim.job_id = top_paying_jobs.job_id
 INNER JOIN skills_dim ON skills_dim.skill_id = skills_job_dim.skill_id
+ORDER BY
+    salary_year_avg DESC
+LIMIT 10
 ;
+
+/*
+Here's the breakdown of the most demanded skills for data analysts in 2023
+SQL and Excel remain fundamental, emphasizing the need for strong foundational skills in data processing and spreadsheet manipulation.
+Programming and Visualization Tools like Python, Tableau, and Power BI are essential, pointing towards the increasing importance of technical skills in data storytelling and decision support.
+
+[
+  {
+    "skills": "sql",
+    "demand_count": "7291"
+  },
+  {
+    "skills": "excel",
+    "demand_count": "4611"
+  },
+  {
+    "skills": "python",
+    "demand_count": "4330"
+  },
+  {
+    "skills": "tableau",
+    "demand_count": "3745"
+  },
+  {
+    "skills": "power bi",
+    "demand_count": "2609"
+  }
+]
+*/
---
+++ SQL/Project/3_top_demanded_skills.sql
@@ -0,0 +1,8 @@
+/*
+Question: What are the most in-demand skills for data analyst?
+- Join job postings to inner join table similar to query 2
+- Identify the top 5 in-demand skills for a data-analyst.
+- Focus on all job postings.
+- Why? Retrives the top 5 skills with the highest demand in job market,
+    provides insights into the most valuable skills for job seekers.
+*/
