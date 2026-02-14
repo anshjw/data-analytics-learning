d276575cf309b73067be28740a68818651a0c85b
anshjw <anshjw1926@gmail.com>
2026-02-14 22:16:58 +0530

    Query Two SQL

---
+++ SQL/Project/2_top_paying_job_skills.sql
@@ -0,0 +1,7 @@
+/*
+Question: What skills are required for the top-paying data analyst jobs?
+- Use the top 10 highest paying Data analyst job from first query
+- Add the specific skills required for this role
+- Why? It provides a detailed look at which high-paying jobs demand certain skills,
+    help helping job seekers understand which skills to develop that align with top salaries
+*/
