7f6ea844f7083fda77d5a47a623b3471b10fb74d
anshjw <anshjw1926@gmail.com>
2026-02-14 22:38:17 +0530

    Fixes in query SQL

--- SQL/Project/2_top_paying_job_skills.sql
+++ SQL/Project/2_top_paying_job_skills.sql
@@ -10,12 +10,10 @@ WITH top_paying_jobs AS (
         job_id,
         job_title,
         salary_year_avg,
-        skills,
         name AS company
     FROM
         job_postings_fact
     LEFT JOIN company_dim ON company_dim.company_id = job_postings_fact.company_id
-    LEFT JOIN skills_dim ON company_dim.skill_id = skills_dim.skill_id
     WHERE
         job_title_short = 'Data Analyst' AND
         job_location = 'Anywhere' AND
@@ -25,8 +23,10 @@ WITH top_paying_jobs AS (
     LIMIT 10
 )
 
-SELECT *
-FROM top_paying_jobs;
-
-SELECT *
-FROM company_dim
+SELECT
+    top_paying_jobs.*
+FROM
+    top_paying_jobs
+INNER JOIN skills_job_dim ON skills_job_dim.job_id = top_paying_jobs.job_id
+INNER JOIN skills_dim ON skills_dim.skill_id = skills_job_dim.skill_id
+;
