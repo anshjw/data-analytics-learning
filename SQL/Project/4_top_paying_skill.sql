9d4261a87d312dc739cf4c1607aa130bb80c2d01
anshjw <anshjw1926@gmail.com>
2026-02-15 11:14:35 +0530

    Result of SQL Project query

---
+++ SQL/Project/4_top_paying_skill.sql
@@ -0,0 +1,8 @@
+/*
+Answer: What are top skills based on salary?
+- Look at the average salary associated with each skill for Data analyst postions
+- Focuses on roles with specified salaries, regardless of location
+- Why? It reveals how different skills impact salary levels for Data Analyst,
+    and helps identify the most financially rewarding skills to acquire or improve.
+*/
+
